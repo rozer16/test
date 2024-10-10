8731e906cdd7f8deb61c442247494b952cf78f29
rozer16 <ravimalkan@gmail.com>
2024-10-10 23:17:58 +0530

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -1,3 +1,34 @@
+/*
+Here is the text from the screenshot you provided:
+
+Challenge 1 description
+
+You are given a string letters made of N English letters. Count the number of different letters that appear in both uppercase and lowercase where all lowercase occurrences of the given letter appear before any uppercase occurrence.
+
+For example, for letters = "aaAbcCABBc" the answer is 2. The condition is met for letters ‘a’ and ‘b’, but not for ‘c’.
+
+Write a function:
+
+class Solution { public int solution(String letters); }
+
+that, given a string letters, returns the number of different letters fulfilling the conditions above.
+
+Examples:
+
+	1.	Given letters = "aaAbcCABBc", the function should return 2, as explained above.
+	2.	Given letters = "xyxZYZabcABC", the function should return 6.
+	3.	Given letters = "ABCabcAFegF", the function should return 0.
+
+Write an efficient algorithm for the following assumptions:
+
+	•	N is an integer within the range [1..100,000];
+	•	string letters is made only of letters (a-z and/or A-Z).
+
+Let me know if you need further clarifications!
+
+*/
+
+
 public class Solution {
     public int solution(String letters) {
         // Boolean arrays to track seen lowercase and valid letters
